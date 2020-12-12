d0e1904510211c48f894b49d93e1e2d9de68c72c
sureshchouhan1991@gmail.com <sureshchouhan1991@gmail.com>
2020-12-12 22:38:10 +0530

    footer change

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -85,28 +85,6 @@ class Contact extends Component {
 						   <span>{phone}</span>
 					   </p>
 				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                       
-                        </span>
-                       
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                
-                        </span>
-                   
-                     </li>
-                  </ul>
-		         </div>
             </aside>
       </div>
    </section>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 Suresh Kumar</li>
+              <li>Design by Suresh</li>
            </ul>
 
         </div>
